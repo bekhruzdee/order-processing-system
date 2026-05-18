2f6fd014a071b76fd6f1783930002bd184e92784
Behruz B. Qutbidinov <behruz.qutbidinov@uic.local>
2026-05-18 17:25:41 +0500

    update user.service

--- apps/user-service/src/user-service.service.ts
+++ apps/user-service/src/user-service.service.ts
@@ -3,7 +3,9 @@ import {
   BadRequestException,
   NotFoundException,
 } from '@nestjs/common';
+import { Prisma } from './generated/prisma';
 import { PrismaService } from './prisma/prisma.service';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class UserService {
@@ -12,12 +14,58 @@ export class UserService {
   async createUser(data: { name: string; email: string }) {
     const { name, email } = data;
 
-    if (!name) throw new BadRequestException('name is required');
-    if (!email) throw new BadRequestException('email is required');
+    if (!name) {
+      throw new RpcException({
+        status: 'error',
+        message: 'name is required',
+      });
+    }
 
-    return this.prisma.user.create({
-      data: { name, email },
-    });
+    if (!email) {
+      throw new RpcException({
+        status: 'error',
+        message: 'email is required',
+      });
+    }
+
+    try {
+      const user = await this.prisma.user.create({
+        data: { name, email },
+      });
+
+      return {
+        status: 'success',
+        message: 'User created successfully',
+        data: user,
+      };
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          return {
+            status: 'error',
+            message: 'Email already exists',
+          };
+        }
+
+        return {
+          status: 'error',
+          message: error.message,
+          code: error.code,
+        };
+      }
+
+      if (error instanceof Error) {
+        return {
+          status: 'error',
+          message: error.message,
+        };
+      }
+
+      return {
+        status: 'error',
+        message: 'Internal server error',
+      };
+    }
   }
 
   async getUsers() {
